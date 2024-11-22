307473d6c08616e0487b8ca06835748cef2ec6e9
Arijit2003 <arijitmodak2003@gmail.com>
2024-11-22 20:26:30 +0530

    Loading of CSS and JS

--- app2.js
+++ app2.js
@@ -14,6 +14,7 @@ app.set('view engine','ejs')
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(express.static("public"))
 
 app.get('/',(req,res)=>{
     res.render('index')
